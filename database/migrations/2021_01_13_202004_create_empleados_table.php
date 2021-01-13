73ff939a44ad343070cc692277d5712816de04ea
andresgalvis26 <andres.felipe99@hotmail.com>
2021-01-13 18:51:36 -0500

    Commit inicial

---
+++ database/migrations/2021_01_13_202004_create_empleados_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Nombre');
+            $table->string('Apellido');
+            $table->integer('Edad');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+}
